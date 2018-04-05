4ea588f0543c975c33225467f7839c75f52c2376
john andrey <andrey_201015@hotmail.com>
2018-04-04 22:14:31 -0500

    Perfil de user

--- src/user/template.js
+++ src/user/template.js
@@ -10,11 +10,11 @@ export default function userPageTemplate(user){
       <div class="col s12 m10 offset-m1 l8 offset-l2 center-align heading">
         <div class="row">
           <div class="col s12 m10 offset-m1 l3 offset-l3 center">
-            <img src="${user.avatar}" class="responsive-img circle" />
+            <img src="${user.avatar}" alt="${user.name}" class="responsive-img circle" />
           </div>
           <div class="col s12 m10 offset-m1 l6 left-align">
-            <h2 class="hide-on-large-only center-align">${user.username}</h2>
-            <h2 class="hide-on-med-and-down left-align">${user.username}</h2>
+            <h2 class="hide-on-large-only center-align">${user.name}</h2>
+            <h2 class="hide-on-med-and-down left-align">${user.name}</h2>
           </div>
         </div>
       </div>
@@ -22,15 +22,15 @@ export default function userPageTemplate(user){
         ${user.pictures.map(function(picture){
           return yo`<div class="col s12 m6 l4">
             <a href="/${user.username}/${picture.id}" class="modal-trigger picture-container">
-              <img src="${picture.src})" class="picture" />
-              <div class="likes"><i class="fa fa-heart"></i> ${picture.likes}</div>
+              <img src="${picture.src}" class="picture" />
+              <div class="likes"><i class="fa fa-heart"></i> ${picture.likes || 0}</div>
             </a>
             <div id="modal${picture.id}" class="modal modal-fixed-footer">
               <div class="modal-content center">
-                <img src="${picture.src})" />
+                <img src="${picture.src}" />
               </div>
               <div class="modal-footer">                                    
-                <div class="btn btn-flat likes"><i class="fa fa-heart"></i> ${translate.message('likes', { likes: picture.likes })}</div>
+                <div class="btn btn-flat likes"><i class="fa fa-heart"></i> ${translate.message('likes', { likes: picture.likes || 0})}</div>
               </div>
             </div>
           </div>`;
